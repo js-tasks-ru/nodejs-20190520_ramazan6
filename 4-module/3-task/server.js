02a91bd4596d8f6633f5948d7c523a29c38c5b9e
romaldowoho <roma.ushpayev@gmail.com>
2019-06-10 19:36:01 -0700

    4 module 3 task solution

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,7 +12,17 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
-
+    	fs.unlink(filepath, (err) => {
+    		if (err) {
+    			if(err.code === 'ENOENT') {
+    				res.statusCode = 404;
+    				res.end('File not found');
+    			}
+    		} else {
+    			res.statusCode = 200;
+    			res.end();
+    		}
+    	});
       break;
 
     default:
