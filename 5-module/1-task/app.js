59e7891602e057e3dd03125c273fea6aa998b6af
romaldowoho <roma.ushpayev@gmail.com>
2019-06-14 17:10:18 -0700

    5-module 1-task solution

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -7,10 +7,27 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+var subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+	await new Promise(function(resolve, reject) {
+		ctx.resolve = resolve;
+		subscribers.push(ctx);
+	})
 });
 
 router.post('/publish', async (ctx, next) => {
+    if (!ctx.request.body.message) {
+	  ctx.status = 400;
+	  return;
+	}
+
+	for (let i = 0; i < subscribers.length; i++) {
+		subscribers[i].body = ctx.request.body.message;
+		subscribers[i].resolve();
+	}
+	ctx.status = 201;
+	subscribers.length = 0;
 });
 
 app.use(router.routes());
